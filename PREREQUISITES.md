00de3b1cc767f14bcc005d0d41ffbd8a89388750
Helain Schoonjans <helain90@hotmail.com>
2025-06-24 11:21:18 +0200

    Chatbot script but not yet working due to SCPs

---
+++ PREREQUISITES.md
@@ -0,0 +1,17 @@
+# Installation steps
+
+- Install VS Code
+- Install python 3.12.4
+- Install AWS CLI
+- Install AWS Toolkit and AWS Boto3
+- Configure IAM Role for VS Code `aws configure` and test with `aws s3 ls`
+- Download Anaconda Nagivator
+- Open VS Code from Anaconda Navigator
+- Install python dependencies
+- pip install boto3==1.37.11
+- pip install langchain==0.3.0
+- pip install -U langchain-aws
+- pip install streamlit==1.43.1
+- pip install transformers
+- pip install PyYAML
+- Go on the AWS Console and request the required model in your target region.
---
+++ chatbot_backend.py
@@ -0,0 +1,35 @@
+#1 import the ConversationSummaryBufferMemory, ConversationChain, ChatBedrock or ChatBedrockConverse Langchain Modules
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chains import ConversationChain
+from langchain_aws import ChatBedrockConverse
+
+#2a Write a function for invoking model- client connection with Bedrock with profile, model_id & Inference params- model_kwargs
+def demo_chatbot(messages):
+    demo_llm = ChatBedrockConverse(
+        credentials_profile_name='default',
+        # uses the arn to specify the region explicitely
+        # might require to adapt the SCPs: https://aws.amazon.com/blogs/machine-learning/enable-amazon-bedrock-cross-region-inference-in-multi-account-environments/
+        model="arn:aws:bedrock:eu-west-1:658245006896:inference-profile/eu.amazon.nova-micro-v1:0",
+        #model="amazon.nova-micro-v1:0",
+        provider='amazon',
+        temperature=0.1,
+        region_name='eu-west-1',
+        max_tokens=1000
+    )
+    return demo_llm.invoke(messages)
+#2b Test out the LLM with invoke method 
+messages = [
+    {
+        "role": "user",
+        "content": [{"text": 'What is an LLM'}]
+    }
+]
+response = demo_chatbot(messages)
+print(response)
+#3 Create a Function for ConversationBufferMemory (llm and max token limit)
+#4 Create a Function for Conversation Chain - Input text + Memory
+#5 Chat response using invoke (Prompt template)
+#Links :
+#https://docs.aws.amazon.com/bedrock/latest/userguide/conversation-inference-call.html
+#https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Converse.html
+#https://docs.aws.amazon.com/bedrock/latest/userguide/conversation-inference-call.htm
