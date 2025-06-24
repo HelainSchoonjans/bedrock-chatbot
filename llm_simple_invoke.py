427e14b86ab097738bcece20d3f0434085fbbe87
Helain Schoonjans <helain90@hotmail.com>
2025-06-24 11:49:34 +0200

    Simple chat backend with memory

--- llm_simple_invoke.py
+++ llm_simple_invoke.py
@@ -1,11 +1,13 @@
+# this scripts makes a simple text exchange with a LLM
+
 #1 import the ConversationSummaryBufferMemory, ConversationChain, ChatBedrock or ChatBedrockConverse Langchain Modules
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain.chains import ConversationChain
 from langchain_aws import ChatBedrockConverse
 
 #2a Write a function for invoking model- client connection with Bedrock with profile, model_id & Inference params- model_kwargs
-def chat_model():
-    model = ChatBedrockConverse(
+def demo_chatbot(messages):
+    demo_llm = ChatBedrockConverse(
         credentials_profile_name='default',
         # cross-region models might require to adapt the SCPs: https://aws.amazon.com/blogs/machine-learning/enable-amazon-bedrock-cross-region-inference-in-multi-account-environments/
         # here I'm using a single region alternative
@@ -15,24 +17,16 @@ def chat_model():
         region_name="eu-west-1", 
         max_tokens=1000
     )
-    return model
-#3 Create a Function for ConversationBufferMemory (llm and max token limit)
-def conversation_memory():
-    memory = ConversationSummaryBufferMemory(
-        llm=chat_model(),
-        max_token_limit=1000
-    )
-    return memory
-#4 Create a Function for Conversation Chain - Input text + Memory
-def conversation_chain(input_text, memory):
-    conversation_chain=ConversationChain(
-        llm=chat_model(),
-        memory=memory,
-        verbose=True
-    )
-    #5 Chat response using invoke (Prompt template)
-    chat_reply = conversation_chain.invoke(input_text)
-    return chat_reply['response']
+    return demo_llm.invoke(messages)
+#2b Test out the LLM with invoke method 
+messages = [
+    {
+        "role": "user",
+        "content": [{"text": 'What is an LLM'}]
+    }
+]
+response = demo_chatbot(messages)
+print(response)
 #Links :
 #https://docs.aws.amazon.com/bedrock/latest/userguide/conversation-inference-call.html
 #https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Converse.html
